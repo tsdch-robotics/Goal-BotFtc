8e3934b125fb09f2189a98246a49c1e21763a966
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2021-01-21 16:04:25 -0500

    Update EasyOpenCVExample.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCVExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCVExample.java
@@ -39,7 +39,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Autonomous
-public class EasyOpenCVExample extends LinearOpMode {
+public class EasyOpenCVExample<position> extends LinearOpMode {
     private static DcMotor FrontLeftMotor;
     private static DcMotor FrontRightMotor;
     private static DcMotor BackLeftMotor;
@@ -49,6 +49,7 @@ public class EasyOpenCVExample extends LinearOpMode {
     int milliseconds = 0;
     double LeftPower = 0;
     double RightPower = 0;
+    int call = 0;
     OpenCvInternalCamera phoneCam;
     SkystoneDeterminationPipeline pipeline;
 
@@ -65,19 +66,16 @@ public class EasyOpenCVExample extends LinearOpMode {
         // landscape orientation, though.
         phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
 
-        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
-                phoneCam.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            public void onOpened() {
+                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
             }
         });
 
         waitForStart();
 
-        while (opModeIsActive())
-        {
+        while (opModeIsActive()) {
             telemetry.addData("Analysis", pipeline.getAnalysis());
             telemetry.addData("Position", pipeline.position);
             telemetry.update();
@@ -87,13 +85,11 @@ public class EasyOpenCVExample extends LinearOpMode {
         }
     }
 
-    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
-    {
+    public static class SkystoneDeterminationPipeline extends OpenCvPipeline {
         /*
          * An enum to define the skystone position
          */
-        public enum RingPosition
-        {
+        public enum RingPosition {
             FOUR,
             ONE,
             NONE
@@ -108,13 +104,13 @@ public class EasyOpenCVExample extends LinearOpMode {
         /*
          * The core values which define the location and size of the sample regions
          */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181,98);
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(181, 98);
 
         static final int REGION_WIDTH = 35;
         static final int REGION_HEIGHT = 25;
 
-        final int FOUR_RING_THRESHOLD = 150;
-        final int ONE_RING_THRESHOLD = 135;
+        static final int FOUR_RING_THRESHOLD = 150;
+        static final int ONE_RING_THRESHOLD = 135;
 
         Point region1_pointA = new Point(
                 REGION1_TOPLEFT_ANCHOR_POINT.x,
@@ -138,25 +134,22 @@ public class EasyOpenCVExample extends LinearOpMode {
          * This function takes the RGB frame, converts to YCrCb,
          * and extracts the Cb channel to the 'Cb' variable
          */
-        void inputToCb(Mat input)
-        {
+        void inputToCb(Mat input) {
             Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
             Core.extractChannel(YCrCb, Cb, 1);
         }
 
         @Override
-        public void init(Mat firstFrame)
-        {
+        public void init(Mat firstFrame) {
             inputToCb(firstFrame);
 
             region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
         }
 
         @Override
-        public Mat processFrame(Mat input)
-        {
+        public Mat processFrame(Mat input) {
             inputToCb(input);
-
+            int call = 0;
             avg1 = (int) Core.mean(region1_Cb).val[0];
 
             Imgproc.rectangle(
@@ -167,12 +160,15 @@ public class EasyOpenCVExample extends LinearOpMode {
                     2); // Thickness of the rectangle lines
 
             position = RingPosition.FOUR; // Record our analysis
-            if(avg1 > FOUR_RING_THRESHOLD){
+            if (avg1 > FOUR_RING_THRESHOLD) {
                 position = RingPosition.FOUR;
-            }else if (avg1 > ONE_RING_THRESHOLD){
+                call = 4;
+            } else if (avg1 > ONE_RING_THRESHOLD) {
                 position = RingPosition.ONE;
-            }else{
+                call = 1;
+            } else {
                 position = RingPosition.NONE;
+                call = 0;
             }
 
             Imgproc.rectangle(
@@ -185,48 +181,120 @@ public class EasyOpenCVExample extends LinearOpMode {
             return input;
         }
 
-        public int getAnalysis()
-        {
+        public int getAnalysis() {
             return avg1;
         }
     }
-
-
-    public void A_Zone(){
-        FrontLeftMotor = hardwareMap.dcMotor.get("DriveFrontLeft");
-        FrontRightMotor = hardwareMap.dcMotor.get("DriveFrontRight");
-        BackLeftMotor = hardwareMap.dcMotor.get("DriveBackLeft");
-        BackRightMotor = hardwareMap.dcMotor.get("DriveBackRight");
-        ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
-        BackRightMotor.setDirection(DcMotor.Direction.REVERSE);
-        DriveRobot(3935, .5,.5,0);//move forward 80.75 in
-        sleep(250);//wait for 1 sec
-        DriveRobot(650,0,0,-1);//move the arm down to horizontal
-        sleep(250);//wait for 1 sec
-        DriveRobot(260, .5,.5,0);//move forward 5 in
-        sleep(250);//wait for 1 sec
-        DriveRobot(650,0,0,1);//move the arm up to vertical
-        sleep(250);//wait for 1 sec
-        DriveRobot(2900, 0.5, -0.5, 0);//turn back
-        sleep(250);//wait for 1 sec
-        DriveRobot(4195, .5,.5,0);//move forward 80.75 in
-        sleep(250);//wait for 1 sec
-        DriveRobot(650,0,0,-1);//move the arm down to horizontal
-        sleep(250);//wait for 1 sec
-        DriveRobot(520, -.5,-.5,0);//move backwards 10 in
-        sleep(250);//wait for 1 sec
-        DriveRobot(650,0,0,1);//move the arm up to vertical
-        sleep(250);//wait for 1 esc
-        DriveRobot(3935, -.5,-.5,0);//move backward 80.75 in
-        sleep(250);//wait for 1 sec
-        DriveRobot(2900, -0.5, 0.5, 0);//turn back
-        sleep(250);//wait for 1 sec
-        DriveRobot(650,0,0,-1);//move the arm down to horizontal
-        sleep(250);//wait for 1 sec
-        DriveRobot(260, .5,.5,0);//move forward 5 in
-        DriveRobot(650,0,0,1);//move the arm up to vertical
+    public void Targetzones() {
+        if (call == 0) {
+            FrontLeftMotor = hardwareMap.dcMotor.get("DriveFrontLeft");
+            FrontRightMotor = hardwareMap.dcMotor.get("DriveFrontRight");
+            BackLeftMotor = hardwareMap.dcMotor.get("DriveBackLeft");
+            BackRightMotor = hardwareMap.dcMotor.get("DriveBackRight");
+            ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
+            //FrontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            //FrontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            BackRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            DriveRobot(3935, .5,.5,.5, .5, 0);//move forward 80.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0,0,0, 0, -1);//move the arm down to horizontal
+            sleep(250);//wait for 1 sec
+            DriveRobot(260, .5,.5,.5, .5, 0);//move forward 5 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0, 0,0,0, 1);//move the arm up to vertical
+            sleep(250);//wait for 1 sec
+            DriveRobot(2900, 0.5, .5,-.5,-0.5, 0);//turn back
+            sleep(250);//wait for 1 sec
+            DriveRobot(4195, .5, .5,.5,.5, 0);//move forward 80.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0,0,0, 0, -1);//move the arm down to horizontal
+            sleep(250);//wait for 1 sec
+            DriveRobot(520, -.5, -.5,-.5,-.5, 0);//move backwards 10 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0, 0,0,0, 1);//move the arm up to vertical
+            sleep(250);//wait for 1 esc
+            DriveRobot(3935, -.5,-.5,-.5, -.5, 0);//move backward 80.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(2900, -.5,-.5,.5, .5, 0);//turn back
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0, 0,0,0, -1);//move the arm down to horizontal
+            sleep(250);//wait for 1 sec
+            DriveRobot(260, .5, .5,.5,.5, 0);//move forward 5 in
+            DriveRobot(650, 0,0,0, 0, 1);//move the arm up to vertical
+        }
+        if (call == 1) {
+            FrontLeftMotor = hardwareMap.dcMotor.get("DriveFrontLeft");
+            FrontRightMotor = hardwareMap.dcMotor.get("DriveFrontRight");
+            BackLeftMotor = hardwareMap.dcMotor.get("DriveBackLeft");
+            BackRightMotor = hardwareMap.dcMotor.get("DriveBackRight");
+            ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
+            //FrontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+            //FrontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            BackRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            DriveRobot(1182, .5,.5,.5, .5, 0);//move forward 22.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(1350, 0.5,.5,-.5, -0.5, 0);//turn right
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(1182, .5,.5,.5, .5, 0);//move forward 22.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(1350, -0.5,-.5,.5, 0.5, 0);//turn left
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(3935, .5, .5,.5,.5, 0);//move forward 80.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0,0,0, 0, -1);//move the arm down to horizontal
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(260, .5,.5,.5, .5, 0);//move forward 5 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0,0,0, 0, 1);//move the arm up to vertical
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(1182, -.5,-.5,-.5, -.5, 0);//move backwards 22.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(600, 0,0,0, 0, -1);//move the arm down to horizontal
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(3935, -.5,-.5,-.5, -.5, 0);//move backwards 80.75 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(600, 0,0,0, 0, 1);//move the arm up to vertical
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(5195, .5,.5,.5, .5, 0);//move forward 100 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0,0,0, 0, -1);//move the arm down to horizontal
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(260, .5,.5,.5,.5, 0);//move forward 5 in
+            sleep(250);//wait for 1 sec
+            DriveRobot(650, 0,0,0,0, 1);//move the arm up to vertical
+            sleep(250);//wait for 0.25 sec
+            DriveRobot(1182, -.5, -.5,-.5,-.5, 0);//move backwards 22.75 in
+            sleep(250);//wait for 1 sec
+        }
+        if (call == 4) {
+            DriveRobot(571, -1,1,1,-1,0);//strafe left 11 in
+            sleep(250);
+            DriveRobot(6117, .5,.5,.5,.5,0);//move forward 117.75 in
+            sleep(250);
+            DriveRobot(650,0,0,0,0,-1);//move the arm down to horizontal
+            sleep(250);
+            DriveRobot(260, .5,.5,.5,.5,0);//move forward 5 in
+            sleep(250);
+            DriveRobot(1753, 1,-1,-1,1,0);//strafe right 33.75 in
+            sleep(250);
+            DriveRobot(6117, -.5,-.5,-.5,-.5,0);//move backward 117.75 in
+            sleep(250);
+            DriveRobot(650,0,0,0,0,1);//move the arm up to vertical
+            sleep(250);
+            DriveRobot(1753, -1,1,1,-1,0);//strafe left 33.75 in
+            sleep(250);
+            DriveRobot(6117, .5,.5,.5,.5,0);//move forward 117.75 in
+            sleep(250);
+            DriveRobot(650,0,0,0,0,-1);//move the arm down to horizontal
+            sleep(250);
+            DriveRobot(260, .5,.5,.5,.5,0);//move forward 5 in
+            sleep(250);
+            DriveRobot(650,0,0,0,0,1);//move the arm up to vertical
+            sleep(250);
+            DriveRobot(1558, -.5,-.5,-.5,-.5, 0);//move backward 30 in
+        }
     }
-    private void DriveRobot(int milliseconds, double LeftPower, double RightPower, double ArmPower) {
+    private void DriveRobot(int milliseconds, double LeftFrontPower, double LeftBackPower, double RightFrontPower, double RightBackPower, double ArmPower) {
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
@@ -239,10 +307,10 @@ public class EasyOpenCVExample extends LinearOpMode {
 
         // set both motors to x power.
 
-        FrontLeftMotor.setPower(LeftPower);
-        FrontRightMotor.setPower(RightPower);
-        BackLeftMotor.setPower(LeftPower);
-        BackRightMotor.setPower(RightPower);
+        FrontLeftMotor.setPower(LeftFrontPower);
+        FrontRightMotor.setPower(RightFrontPower);
+        BackLeftMotor.setPower(LeftBackPower);
+        BackRightMotor.setPower(RightBackPower);
         ArmMotor.setPower(ArmPower);
 
 

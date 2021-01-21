843d750e80a7ace721eda75126d30d70b4336342
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2021-01-21 16:09:57 -0500

    Update WobbleBlue_B.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleBlue_B.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleBlue_B.java
@@ -33,8 +33,6 @@ public class WobbleBlue_B extends LinearOpMode {
         BackLeftMotor = hardwareMap.dcMotor.get("DriveBackLeft");
         BackRightMotor = hardwareMap.dcMotor.get("DriveBackRight");
         ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
-        //FrontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        //FrontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         BackRightMotor.setDirection(DcMotor.Direction.REVERSE);
         DriveRobot(1182, .5,.5,.5, .5, 0);//move forward 22.75 in
         sleep(250);//wait for 1 sec

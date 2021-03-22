98296d6db7c40f881d4c56e056d551a654496b2b
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2021-03-22 13:01:52 -0400

    Update TankDriveTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TankDriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TankDriveTeleop.java
@@ -137,7 +137,6 @@ public class TankDriveTeleop extends OpMode {
         } else {
             robot.LauncherMotor.setPower(0);
         }
-<<<<<<< Updated upstream
         if (gamepad1.b) {
             robot.servo.setPosition(0.0);
             //grabStatus = "close";
@@ -146,13 +145,12 @@ public class TankDriveTeleop extends OpMode {
             //grabStatus = "open";
         }
         robot.setDriveMotors(DriveLeftPower, DriveRightPower, DriveLeftPower, DriveRightPower);
-=======
+
         if (gamepad1.y) {
             robot.TestMotor.setPower(1);
         }else {
             robot.TestMotor.setPower(0);
         }
->>>>>>> Stashed changes
     }
 }
 

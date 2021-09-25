66d15f7a9d3831aa75eacd088ca76a9aecaa65c8
TSDCH Robotics (LabPC-5459) <LabPC-5459@robotics.tsdch>
2021-09-25 09:52:13 -0400

    Update Jeff_Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Jeff_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Jeff_Autonomous.java
@@ -9,16 +9,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Jeff_Autonomous extends LinearOpMode {
     DcMotor FrontLeftMotor;
     DcMotor FrontRightMotor;
+    DcMotor BackRightMotor;
 
     public void runOpMode() {
         FrontLeftMotor = hardwareMap.dcMotor.get("LeftMotor");
         FrontRightMotor = hardwareMap.dcMotor.get("RightMotor");
+        BackRightMotor = hardwareMap.dcMotor.get("BackRightMotor");
         DriveRobot(500, 0.75, 0.75);//move forward
         sleep(250);//wait for 0.25 sec
+        DriveRobot(400, 1, 0);
+        sleep(250);
+        DriveRobot(500,-1,-1);
     }
 
 
-    private void DriveRobot(int milliseconds, double LeftPower, double RightPower) {
+    public void DriveRobot(int milliseconds, double LeftPower, double RightPower) {
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
@@ -36,13 +41,13 @@ public class Jeff_Autonomous extends LinearOpMode {
         FrontLeftMotor.setPower(LeftPower);
         FrontRightMotor.setPower(RightPower);
 
-
         sleep(milliseconds);        // wait for x seconds.
 
         // set motor power to zero to stop motors.
 
         FrontLeftMotor.setPower(0);
         FrontRightMotor.setPower(0);
+        BackRightMotor.setPower(0);
     }
 }
 

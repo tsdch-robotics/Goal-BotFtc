faa5a269c3d15d9aaa901e03a63f7c4d65f25cb9
TSDCH Robotics (LabPC-5459) <LabPC-5459@robotics.tsdch>
2021-09-27 14:07:42 -0400

    Update XyhVector.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/XyhVector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/XyhVector.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 public class XyhVector {
+    public double x;
+    public double y;
+    public double h;
+
+    public XyhVector(int x, int y, double h) {
+    }
+
+    public XyhVector(XyhVector START_POS) {
+    }
 }

2f2bf6d40cb82e1f7c5e6a1e900b04134844c003
TSDCH Robotics (LabPC-5459) <LabPC-5459@robotics.tsdch>
2021-11-20 11:58:01 -0500

    Create OdometryTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/OdometryTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Autonomous.ChampBot;
+
+public class OdometryTest {
+    ChampBot robot = new ChampBot();
+     /*public void Directionvector(float Math.cos(h),float Math.sin(h)){
+
+    }
+    public void target (float x1, float y1){
+
+    }
+    public void targetvector = (x1-x,y1-y)
+    public D=((x1-x)**2+(y1-y)**2)**(1/2)
+
+    if (gamepad2.b) {
+        float x1=213;
+        float y1=102;
+    }
+    */
+}

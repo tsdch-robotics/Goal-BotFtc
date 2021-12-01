9d0339f93d53d74bd26a1e6483589425c6417f2d
TSDCH Robotics (LabPC-5459) <LabPC-5459@robotics.tsdch>
2021-12-01 09:42:14 -0500

    Create RumbleTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RumbleTest.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@TeleOp(name="RumbleTest", group = "ChampBot")
+public class RumbleTest extends LinearOpMode
+{
+    boolean lastA = false;
+    boolean lastLB = false;
+    boolean highLevel =false;
+    boolean secondHalf = false;
+
+    Gamepad.RumbleEffect customRumbleEffect;
+    ElapsedTime runtime = new ElapsedTime();
+
+    final double HALF_TIME = 60.0;
+    final double TRIGGER_THRESHHOLD = 0.75;
+
+    @Override
+    public void runOpMode()
+    {
+        customRumbleEffect = new Gamepad.RumbleEffect.Builder()
+            .addStep(0.0, 1.0, 500)
+            .addStep(0.0, 0.0, 300)
+            .addStep(1.0, 0.0, 250)
+            .addStep(0.0, 0.0, 250)
+            .addStep(1.0, 0.0, 250)
+            .build();
+
+        telemetry.addData(">", "Press Start");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive())
+        {
+            boolean currentA = gamepad1.a;
+            boolean currentLB = gamepad1.left_bumper;
+
+            telemetry.addData(">", "Are we RUMBLING? %s\n", gamepad1.isRumbling()? "YES" : "no");
+
+            if (runtime.seconds() > HALF_TIME && !secondHalf) {
+                gamepad1.runRumbleEffect(customRumbleEffect);
+                secondHalf = true;
+            }
+            if (!secondHalf) {
+                telemetry.addData(">", "Halftime Alert Countdown: %3.0f Sec \n", HALF_TIME - runtime.seconds());
+            }
+            if (currentLB) {
+                gamepad1.rumble(gamepad1.left_trigger, gamepad1.right_trigger, Gamepad.RUMBLE_DURATION_CONTINUOUS);
+
+                telemetry.addData(">", "Squeeze triggers to control rumbles");
+                telemetry.addData("> : Rumble", "Left: %.0f%%   Right: %.0f%%", gamepad1.left_trigger * 100, gamepad1.right_trigger * 100);
+            } else {
+                if (lastLB) {
+                    gamepad1.stopRumble();
+                }
+            }
+
+        }
+    }
+}

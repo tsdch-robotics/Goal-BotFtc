2cb38e74dad485d1799d15f66d9ddfdb3e6b76a7
TSDCH Robotics (LabPC-5459) <LabPC-5459@robotics.tsdch>
2021-12-01 09:06:07 -0500

    Update TankDriveBernardo.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TankDriveBernardo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TankDriveBernardo.java
@@ -37,6 +37,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import java.lang.*;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
